7d78bbac890ddb09a682d5a6526a26f4fcb60567
Rafael Meyer <rafael.meyer@crmall.com>
2016-10-19 11:31:43 -0200

    first commit

---
+++ app/src/main/java/com/example/rafaelmeyer/myswipeviews/PageFragment.java
@@ -0,0 +1,42 @@
+package com.example.rafaelmeyer.myswipeviews;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by rafael.meyer on 10/19/16.
+ */
+public class PageFragment extends Fragment {
+
+    private static final String ARG_PAGE = "ARG_PAGE";
+    private int myPage;
+
+    public static PageFragment newInstance(int page) {
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + myPage);
+        return view;
+    }
+}
